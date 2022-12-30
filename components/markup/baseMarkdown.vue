7064159dd3ace371f2679301d9a88c7976e36867
thzero <thzero@users.noreply.github.com>
2022-12-30 12:36:45 -0600

    updates

--- components/markup/baseMarkdown.vue
+++ components/markup/baseMarkdown.vue
@@ -1,66 +1,53 @@
 <script>
-import { computed, getCurrentInstance } from 'vue';
+import { computed } from 'vue';
 
 import LibraryConstants from '@thzero/library_client/constants';
 
 import GlobalUtility from '@thzero/library_client/utility/global';
 
-import base from '../base';
+import { useBaseComponent } from '../base';
 
-export default {
-	name: 'baseMarkdown',
-	extends: base,
-	props: {
-		useGithub: {
-			type: Boolean,
-			default: true
-		},
-		// must be included in props
-		modelValue: {
-			type: String,
-			default: ''
-		}
-	},
-	setup (props) {
-		const instance = getCurrentInstance();
+export function useBaseMarkdownComponent(props, context, options) {
+	const {
+		correlationId,
+		error,
+		hasFailed,
+		hasSucceeded,
+		initialize,
+		logger,
+		noBreakingSpaces,
+		notImplementedError,
+		success
+	} = useBaseComponent(props, context, options);
 
-		const serviceMarkup = GlobalUtility.$injector.getService(LibraryConstants.InjectorKeys.SERVICE_MARKUP_PARSER);
+	const serviceMarkup = GlobalUtility.$injector.getService(LibraryConstants.InjectorKeys.SERVICE_MARKUP_PARSER);
 
-		const markdownClass = computed(() => {
-			return 'markdown ' + (props.useGithub ? 'markdown-body' : '');
-		});
+	const markdownClass = computed(() => {
+		return 'markdown ' + (props.useGithub ? 'markdown-body' : '');
+	});
 
-		const display = computed(() => {
-			if (String.isNullOrEmpty(props.modelValue))
-				return '';
+	const display = computed(() => {
+		if (String.isNullOrEmpty(props.modelValue))
+			return '';
 
-			const correlationId = instance.ctx.correlationId();
-			const results = serviceMarkup.render(correlationId, props.modelValue);
-			return serviceMarkup.trimResults(correlationId, results);
-		});
+		const correlationIdI = correlationId();
+		const results = serviceMarkup.render(correlationIdI, props.modelValue);
+		return serviceMarkup.trimResults(correlationIdI, results);
+	});
 
-		return Object.assign(base.setup(props), {
-			display,
-			markdownClass,
-			serviceMarkup
-		});
-	}
-	// computed: {
-	// 	display() {
-	// 		const correlationId = this.correlationId();
-	// 		return this._serviceMarkup.trimResults(correlationId, instance._serviceMarkup.render(correlationId, this.value));
-	// 	}
-	// },
-	// created() {
-	// 	this._serviceMarkup = GlobalUtility.$injector.getService(LibraryConstants.InjectorKeys.SERVICE_MARKUP_PARSER);
-	// }
+	return {
+		correlationId,
+		error,
+		hasFailed,
+		hasSucceeded,
+		initialize,
+		logger,
+		noBreakingSpaces,
+		notImplementedError,
+		success,
+		display,
+		markdownClass,
+		serviceMarkup
+	};
 };
 </script>
-
-<style>
-	@import 'https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.1.0/github-markdown.min.css';
-	.markdown ol, ul, li {
-		margin: revert;
-		padding: revert; /* Padding is what gives the indentation */
-	}
-</style>
